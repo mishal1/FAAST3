06e2b0ec4a671bb4b8994de38978ea1258cf68dc
Mishal Islam <mishal.islam01@gmail.com>
2014-11-07 12:21:28 +0000

    Added method that automatically sends passengers to the station

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -4,16 +4,18 @@ class Passenger
 		@tapped_in = false
 	end
 
-	def tap_into_station
+	def tap_into(station)
 		@tapped_in = true
+		station.hold(self)
 	end
 
 	def tapped_in?
 		@tapped_in
 	end
 
-	def tap_out_of_station
+	def tap_out_of(station)
 		@tapped_in = false
+		station.release(self)
 	end
 
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -3,6 +3,7 @@ require 'passenger'
 describe Passenger do
 
 	let(:passenger) {Passenger.new}
+	let(:station)	{double :station}
 
 	it "should not be tapped into a station when initalized" do
 		expect(passenger.tapped_in?).to be false
@@ -10,14 +11,27 @@ describe Passenger do
 	end
 
 	it "should be able to tap into the station" do
-		passenger.tap_into_station
+		expect(station).to receive(:hold).with(passenger)
+		passenger.tap_into(station)
 		expect(passenger.tapped_in?).to be true
 	end
 
 	it "should be able to leave the station" do
-		passenger.tap_into_station
-		passenger.tap_out_of_station
+		allow(station).to receive(:hold).with(passenger)
+		passenger.tap_into(station)
+		allow(station).to receive(:release).with(passenger)
+		passenger.tap_out_of(station)
 		expect(passenger.tapped_in?).to be false
+	end
+
+	it "should automatically move into the station when tapped in" do
+		expect(station).to receive(:hold).with(passenger)
+		passenger.tap_into(station)
+	end
+
+	it "should automatically move out of the station when tapped out" do
+		expect(station).to receive(:release).with(passenger)
+		passenger.tap_out_of(station)
 	end
 
 end
